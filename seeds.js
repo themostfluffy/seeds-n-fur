42ab30a4c1e4754fa39e9407424b71fd08343400
themostfluffy <Fuzzyfox9876@gmail.com>
2026-02-27 18:46:02 +0000

    jotigvjiu

--- seeds.js
+++ seeds.js
@@ -7,21 +7,18 @@ var roseSeeds = 0;
 var tulipSeeds = 0;
 var pondLilySeeds = 0;
 var potatoSeeds = 0;
-
 //rare seeds
 var venusFlytrapSeeds = 0;
 var cactusSeeds = 0;
 var orchidSeeds = 0;
 var wistariaSeeds = 0;
 var omnivorousPlantSeeds = 0;
-
 //legendary seeds
 var kangarooPawSeeds = 0;
 var corpseFlowerSeeds = 0;
 var ashtulipeSeeds = 0;
 var volcanicPoppySeeds = 0;
 var ghostOrchidSeeds = 0;
-
 //costs 
 //common seeds
 var fernCost = 5;
@@ -43,4 +40,6 @@ var ashtulipeCost = 2000;
 var volcanicPoppyCost = 2500;
 var ghostOrchidCost = 3000;
 
-//images 
+//images
+//for seeds draw a seed packet and call it a day 
+//for the grown plant, draw what you think it would look like but as a furry  :3
